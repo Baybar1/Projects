06e34ba2602aed047c55032e8eb96026f672d846
Branley J Aybar <branleyjraybar@gmail.com>
2022-12-05 21:00:18 -0500

    added backend

--- Animeniac/server/server.js
+++ Animeniac/server/server.js
@@ -6,5 +6,7 @@ app.use(express.json(),express.urlencoded({extended:true}));
 const port = 8000;
 require('./config/anime.config');
 require('./routes/anime.routes')(app)
+require('dotenv').config();
+const jwt = require('jsonwebtoken')
 
 app.listen(port, () => console.log('Locked and Loaded on port 8000'))
